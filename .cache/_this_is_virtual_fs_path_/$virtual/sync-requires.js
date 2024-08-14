d9598467423caf8978c4e97cad4af97aeffb48de
Michael Yeboah <mbaah80@gmail.com>
2024-08-14 14:02:33 +0000

    push

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,16 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/michaelyeboah/Desktop/projects/michael-yeboah/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/michaelyeboah/Desktop/projects/michael-yeboah/src/pages/404.js")),
+  "component---src-pages-archive-js": preferDefault(require("/Users/michaelyeboah/Desktop/projects/michael-yeboah/src/pages/archive.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/michaelyeboah/Desktop/projects/michael-yeboah/src/pages/index.js")),
+  "component---src-pages-pensieve-index-js": preferDefault(require("/Users/michaelyeboah/Desktop/projects/michael-yeboah/src/pages/pensieve/index.js")),
+  "component---src-pages-pensieve-tags-js": preferDefault(require("/Users/michaelyeboah/Desktop/projects/michael-yeboah/src/pages/pensieve/tags.js")),
+  "component---src-templates-post-js": preferDefault(require("/Users/michaelyeboah/Desktop/projects/michael-yeboah/src/templates/post.js")),
+  "component---src-templates-tag-js": preferDefault(require("/Users/michaelyeboah/Desktop/projects/michael-yeboah/src/templates/tag.js"))
+}
+
